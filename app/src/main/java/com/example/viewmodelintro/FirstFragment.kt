4430013ed7e3111102448385b1b463305149c14d
soongonkim <soongon@hucloud.co.kr>
2022-09-02 16:45:21 +0900

    viewmodel + live data(observer pattern)

--- app/src/main/java/com/example/viewmodelintro/FirstFragment.kt
+++ app/src/main/java/com/example/viewmodelintro/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.example.viewmodelintro
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodelintro.databinding.FragmentFirstBinding
 
@@ -27,28 +29,43 @@ class FirstFragment : Fragment() {
 
         viewModel = ViewModelProvider(this).get(FirstViewModel::class.java)
 
+        // observer setting
+        viewModel.messages.observe(viewLifecycleOwner, Observer {
+            theMessage -> binding.firstFragmentTvMessage.text = theMessage
+        })
+        viewModel.clicks.observe(viewLifecycleOwner, Observer {
+            newClicks -> binding.firstFragmentTvClicks.text = "$newClicks time(s) 클릭"
+        })
+
         // set event listener
-        binding.firstFragmentBtHello.setOnClickListener { hello() }
-        binding.firstFragmentBtBye.setOnClickListener { bye() }
+        binding.firstFragmentBtHello.setOnClickListener {
+            viewModel.hello(binding.firstFragmentEtName.text.toString())
+        }
+        binding.firstFragmentBtBye.setOnClickListener {
+            viewModel.bye(binding.firstFragmentEtName.text.toString())
+        }
 
         return binding.root
     }
+    /*
     private fun hello() {
         val yourName = binding.firstFragmentEtName.text.toString()
-        viewModel.messages = "안녕 $yourName 님"
-        viewModel.clicks++
-        updateUI()
+        viewModel.messages.value = "안녕 $yourName 님"
+        viewModel.clicks.value = viewModel.clicks.value?.plus(1)
+        Log.i("FirstFragment", viewModel.clicks.value.toString())
+        //updateUI()
     }
 
     private fun bye() {
         val yourName = binding.firstFragmentEtName.text.toString()
-        viewModel.messages = "잘가요 $yourName 님"
-        viewModel.clicks++
-        updateUI()
+        viewModel.messages.value = "잘가요 $yourName 님"
+        viewModel.clicks.value = viewModel.clicks.value?.plus(1)
+        //updateUI()
     }
-    private fun updateUI() {
-        binding.firstFragmentTvMessage.text = viewModel.messages
-        binding.firstFragmentTvClicks.text = "${viewModel.clicks} time(s) clicked."
-    }
+    */
+//    private fun updateUI() {
+//        binding.firstFragmentTvMessage.text = viewModel.messages
+//        binding.firstFragmentTvClicks.text = "${viewModel.clicks} time(s) clicked."
+//    }
 
 }
--- app/src/main/java/com/example/viewmodelintro/FirstViewModel.kt
+++ app/src/main/java/com/example/viewmodelintro/FirstViewModel.kt
@@ -1,19 +1,31 @@
 package com.example.viewmodelintro
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class FirstViewModel: ViewModel() {
 
-    var clicks = 0
-    var messages = ""
+    val clicks = MutableLiveData<Int>()
+    val messages = MutableLiveData<String>()
 
     init {
+        clicks.value = 0
+        messages.value = ""
         Log.i("FirstViewModel", "first viewmodel created..")
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.i("FirstViewModel", "viewmodel died")
+    }
+
+    fun hello(name: String) {
+        messages.value = "hi $name"
+        clicks.value = clicks.value?.plus(1)
+    }
+    fun bye(name: String) {
+        messages.value = "bye $name"
+        clicks.value = clicks.value?.plus(1)
     }
 }
